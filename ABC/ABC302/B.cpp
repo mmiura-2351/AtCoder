eec58e293dc970e05172548e4e844cf00ef543c6
MiuraManato <manato20030510@icloud.com>
2023-08-12 20:50:04 +0900

    commits

---
+++ ABC/ABC302/B.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int h,w;
+  cin >> h >> w;
+  for (int i = 0; i < h; i++){
+    string s;
+    cin >> s;
+    if (s == "s")
+  }
+  return 0;
+}
