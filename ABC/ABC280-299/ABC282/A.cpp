ce70c0f1aa0d5509e5504bdf7873f54c2e7ee482
manato miura <mmiura2351@gmail.com>
2023-09-05 09:19:14 +0900

    commits

---
+++ ABC/ABC280-299/ABC282/A.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int k;
+  string s = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  cin >> k;
+  for (int i = 0; i < k; i++){
+    cout << s[i];
+  }
+  cout << endl;
+}
