1a2bfee2a32fc1a565ad0190bb2f61251749b03a
manato miura <mmiura2351@gmail.com>
2023-09-07 09:14:52 +0900

    commit

--- ARC/ARC100-119/ARC106/A.cpp
+++ ARC/ARC100-119/ARC106/A.cpp
@@ -1,22 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// 不完全コード　バグあり
+long long lpow(int n, int k) {
+  long long res = 1;
+  for (int i = 0; i < k; i++) {
+    res *= n;
+  }
+  return res;
+}
+
 int main() {
   long long n;
   cin >> n;
 
-  long long pow3 = 1;
-  for (int i = 0; pow3 < n; i++) {
-    long long pow5 = 1;
-    for (int j = 0; pow5 + pow3 < n; j++) {
-      if (pow3 + pow5 == n) {
+  for (int i = 1; i <= 38; i++) {
+    for (int j = 1; j <= 26; j++) {
+      if (lpow(3, i) + lpow(5, j) == n) {
         cout << i << " " << j << endl;
         return 0;
       }
-      pow5 *= 5;
     }
-    pow3 *= 3;
   }
   cout << -1 << endl;
 }
