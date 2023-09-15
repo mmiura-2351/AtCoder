ea3f375f41f61b7efbd463dca4b21386530780ab
manato miura <mmiura2351@gmail.com>
2023-09-15 09:16:17 +0900

    Add macro and template to utils

--- utils.cpp
+++ utils.cpp
@@ -4,9 +4,32 @@ using ll=long long;
 using str=string;
 #define rep(i, n) for (int i = 0; (i) < (n); ++(i))
 #define all(a) (a).begin(), (a).end()
-#define input(a) cin >> (a)
-#define print(p) cout << (p)
-#define println(p) cout << (p) << endl
+#define inputarr(arr, size) do {for (size_t i = 0; i < size; ++i) { cin >> arr[i]; }} while (0)
+#define printarr(arr, size) do {for (size_t i = 0; i < size; ++i) { cout << arr[i]; if (i != size - 1) cout << " ";} cout << endl;} while (0)
+
+template <typename... Args>
+void input(Args&... args) {
+    ((cin >> args), ...);
+}
+
+template <typename... Args>
+void print(Args... args) {
+    ((cout << args << " "), ...);
+}
+
+template <typename... Args>
+void println(Args... args) {
+    print(args...);
+    cout << endl;
+}
+
+
+
+
+
+
+
+
 
 bool contains(const string& haystack, const string& needle) {
     return haystack.find(needle) != string::npos;
