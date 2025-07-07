6e57e89d42333ab8ef26cc8bd5d93002aa9145de
Manato Miura <mmiura2351@gmail.com>
2025-07-07 15:35:05 +0000

    add abc413-c

--- contests/abc413/c/main.py
+++ contests/abc413/c/main.py
@@ -1,46 +1,40 @@
 import sys
+from collections import deque
 input = sys.stdin.readline
 
 def main():
-    def type1(A: list, query):
-        A.append(query)
+    def type1(A: deque, c: int, x: int):
+        A.append((c, x))
         return A
 
-    def type2(A: list, query):
-        # print(A, query)
-        k = int(query[1])
+    def type2(A: deque, k: int):
         sum = 0
         while k != 0:
-            # print(f"{sum=}")
-            # print(A)
-            # print(sum)
-            first_c = int(A[0][1])
-            x = int(A[0][2])
+            first_c, x = A[0]
             if first_c > k:
-                # print("c > k")
-                # first_c = first_c - k
-                A[0][1] = str(first_c - k)
-                sum += x*k
+                A[0] = (first_c - k, x)
+                sum += x * k
                 k = 0
             else:
-                # print("k > c")
                 k -= first_c
-                sum += x*first_c
-                A.pop(0)
+                sum += x * first_c
+                A.popleft()
 
         print(sum)
         return A
 
     q = int(input())
 
-    A = []
+    A = deque()
     for _ in range(q):
         query = input().strip().split()
-        # print(A)
         if query[0] == "1":
-            A = type1(A, query)
+            c = int(query[1])
+            x = int(query[2])
+            A = type1(A, c, x)
         elif query[0] == "2":
-            A = type2(A, query)
+            k = int(query[1])
+            A = type2(A, k)
 
 if __name__ == "__main__":
     main()
